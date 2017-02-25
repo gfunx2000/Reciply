0ecfc7f300cdbb92b4279bfa70223421d8dbc1ee
gfunx2000 <josephsalmond@gmail.com>
2017-02-24 21:17:23 -0700

    Added most icons.

---
+++ View Controllers/settingsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  settingsViewController.swift
+//  Tabbed Reciply
+//
+//  Created by Joseph Salmond on 2/18/17.
+//  Copyright © 2017 Joseph Salmond. All rights reserved.
+//
+
+import UIKit
+
+
+var defaultServings = 4
+
