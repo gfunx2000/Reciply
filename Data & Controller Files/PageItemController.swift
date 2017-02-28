3a988b67dff586af7884ed3f132c106a0347f79c
gfunx2000 <josephsalmond@gmail.com>
2017-02-27 23:34:31 -0700

    Adding to source code

---
+++ Data & Controller Files/PageItemController.swift
@@ -0,0 +1,33 @@
+//
+//  PageItemController.swift
+//  Tabbed Reciply
+//
+//  Created by Joseph Salmond on 2/27/17.
+//  Copyright © 2017 Joseph Salmond. All rights reserved.
+//
+
+import UIKit
+
+class PageItemController: UIViewController {
+    
+    var itemIndex: Int = 0
+    var imageName: String = "" {
+        
+        didSet {
+            
+            if let imageView = contentImageView {
+                imageView.image = UIImage(named: imageName)
+            }
+            
+        }
+    }
+    
+    @IBOutlet var contentImageView: UIImageView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        contentImageView!.image = UIImage(named: imageName)
+        // Add in things about text here
+    }
+    
+}
